3c7f62d59ca59c42716e43c36c3770a31032b6cb
hongquan <lhqhacker@163.com>
2022-08-29 18:32:45 +0800

    添加线上环境

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,8 @@ export default {
         "authSource": null
     },
     "host": {
-        "url": "http://localhost",
+        "url": "http://huajian.smallpig.site", // 线上环境
+        // "url": "http://localhost:3399", // 本地环境
         "port": "3000"
     },
     "jwt": {
