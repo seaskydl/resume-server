41e4339018d425e99b9483afbe756d31c9694cc7
hongquan <lhqhacker@163.com>
2022-09-02 21:32:37 +0800

    添加配置文件示例

---
+++ src/config.example.ts
@@ -0,0 +1,25 @@
+export default {
+  db: {
+    user: "数据库用户名",
+    pass: "数据库密码",
+    host: "数据库地址ip",
+    port: "数据库端口",
+    database: "数据库名称",
+    authSource: null,
+  },
+  host: {
+    url: "配置地址，邮件中携带的链接",
+    port: "端口",
+  },
+  jwt: {
+    secretOrKey: "token加密密钥",
+    expiresIn: 36000000,
+  },
+  mail: {
+    host: "邮件服务其地址",
+    port: "邮件端口",
+    secure: true, // 是否开启安全认证
+    user: "发送邮件的邮箱",
+    pass: "邮箱密码", // 注意不是登录密码，具体需要到邮箱官网里面去设置，可以百度一下nojs发送邮件
+  },
+};
--- src/config.ts
+++ src/config.ts
@@ -1,30 +1,28 @@
 export default {
-    "db": {
-        "user": 'resume',
-        "pass": '20153106l',
-        "host": "110.42.181.210",
-        "port": "27017",
-        "database": "resume",
-        "authSource": null
-    },
-    "host": {
-        "url": "https://huajian.smallpig.site", // 线上环境
-        // "url": "http://localhost:3399", // 本地环境
-        "port": "3399"
-    },
-    "jwt": {
-        "secretOrKey": "smallpig",
-        "expiresIn": 36000000
-    },
-    "mail":{
-        "host": "smtp.163.com",
-        "port": 465,
-        "secure": true,
-        "user": "lhqhacker@163.com",
-        "pass": "FOFMEMXOOXULIQRM"
-    }
-  }
-  
-//   git remote set-url origin https://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@github.com/Hacker233/resume-server.git
+  db: {
+    user: "resume",
+    pass: "20153106l",
+    host: "110.42.181.210",
+    port: "27017",
+    database: "resume",
+    authSource: null,
+  },
+  host: {
+    url: "https://huajian.smallpig.site", // 线上环境
+    // "url": "http://localhost:3399", // 本地环境
+    port: "3399",
+  },
+  jwt: {
+    secretOrKey: "smallpig",
+    expiresIn: 36000000,
+  },
+  mail: {
+    host: "smtp.163.com",
+    port: 465,
+    secure: true,
+    user: "lhqhacker@163.com",
+    pass: "FOFMEMXOOXULIQRM",
+  },
+};
 
-  
+//   git remote set-url origin https://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@github.com/Hacker233/resume-server.git
