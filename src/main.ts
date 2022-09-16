9c78b5b94b362531b930fe07d501205f96e1e6b5
hongquan <lhqhacker@163.com>
2022-09-16 09:17:16 +0800

    添加全局限流

--- src/main.ts
+++ src/main.ts
@@ -22,9 +22,10 @@ async function bootstrap() {
   app.use(helmet());
   app.enableCors();
 
+  // 全局限流
   app.use(
     rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
+      windowMs: 1 * 60 * 1000, // 1 minutes
       max: 100, // limit each IP to 100 requests per windowMs
       message: "Too many requests from this IP, please try again later",
     })
